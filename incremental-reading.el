b2695ea85e0806ec88dc6bc6e4acecd13a17a18c
colawithsauce <cola_with_sauce@foxmail.com>
2021-12-23 09:18:45 +0800

    Update README.

--- incremental-reading.el
+++ incremental-reading.el
@@ -6,7 +6,7 @@
 ;; Version: 0.3
 ;; Keywords: anki anki-editor incremental-reading supermemo
 ;; Homepage: https://github.com/vascoferreira25/incremental-reading
-;; Package-Requires: ((org) (ox-html) (anki-editor "0.3.3") (request "0.3.0") (emacs "26.6"))
+;; Package-Requires: ((org) (org-protocol) (ox-html) (anki-editor "0.3.3") (request "0.3.0") (emacs "26.6"))
 
 ;; This file is not part of GNU Emacs.
 
@@ -20,6 +20,7 @@
 
 ;;; Dependencies:
 (require 'org)
+(require 'org-protocol)
 (require 'ox-html)
 (require 'anki-editor)
 (require 'request)
